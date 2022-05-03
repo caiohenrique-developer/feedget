a46c2c7109a0dbaea252b29224eafa68375cdfe8
caiohenrique-developer <caiorussellrox@msn.com>
2022-05-03 14:09:42 -0300

    🎨 [style]: Create feedback options view

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,5 +1,33 @@
 import { CloseButton } from "./CloseButton";
 
+import bugImageUrl from '../assets/bug.svg';
+import ideaImageUrl from '../assets/idea.svg';
+import thoughtImageUrl from '../assets/thought.svg';
+
+const feedbackTypes = {
+    BUG: {
+        title: 'Problema',
+        image: {
+            source: bugImageUrl,
+            alt: 'Imagem de um inseto'
+        }
+    },
+    IDEA: {
+        title: 'Ideia',
+        image: {
+            source: ideaImageUrl,
+            alt: 'Imagem de uma lâmpada'
+        }
+    },
+    OTHER: {
+        title: 'Outro',
+        image: {
+            source: thoughtImageUrl,
+            alt: 'Imagem de um balão de pensamento'
+        }
+    },
+}
+
 export const WidgetForm = (): JSX.Element => {
   return (
       <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -9,7 +37,36 @@ export const WidgetForm = (): JSX.Element => {
               <CloseButton />
           </header>
           
-          <h1>Widget Form</h1>
+          <div className="flex py-8 gap-2 w-full">
+              {Object.entries(feedbackTypes).map(([key, value]) => {
+                  return (
+                      <button
+                        key={key}
+                        className="
+                            bg-zinc-800 
+                            rounded-lg 
+                            py-5 
+                            w-24 
+                            flex-1 
+                            flex 
+                            flex-col 
+                            items-center 
+                            gap-2 
+                            border-2 
+                            border-transparent 
+                            hover:border-brand-500 
+                            focus:border-brand-500 
+                            focus:outline-none
+                        "
+                        type="button"
+                        onClick={() => {}}
+                      >
+                        <img src={value.image.source} alt={value.image.alt} />
+                        <span>{value.title}</span>
+                      </button>
+                  );
+              })}
+          </div>
 
           <footer className="text-xs text-neutral-400">
               Feito com 🫀 pela <a className="underline underline-offset-2" href="https://rocektseat.com.br">Rocketseat</a>
