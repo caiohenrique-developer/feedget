2cc600ebf3c9537a929c98e23f894b170cb4014c
caiohenrique-developer <caiorussellrox@msn.com>
2022-05-03 14:30:01 -0300

    ✨ [feat]: Manage feedback selected type option

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -3,6 +3,7 @@ import { CloseButton } from "./CloseButton";
 import bugImageUrl from '../assets/bug.svg';
 import ideaImageUrl from '../assets/idea.svg';
 import thoughtImageUrl from '../assets/thought.svg';
+import { useState } from "react";
 
 const feedbackTypes = {
     BUG: {
@@ -28,7 +29,11 @@ const feedbackTypes = {
     },
 }
 
+type FeedbackTYpe = keyof typeof feedbackTypes;
+
 export const WidgetForm = (): JSX.Element => {
+    const [feedbackType, setFeedbackType] = useState<FeedbackTYpe | null>(null)
+    
   return (
       <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
           <header>
@@ -37,36 +42,40 @@ export const WidgetForm = (): JSX.Element => {
               <CloseButton />
           </header>
           
-          <div className="flex py-8 gap-2 w-full">
-              {Object.entries(feedbackTypes).map(([key, value]) => {
-                  return (
-                      <button
-                        key={key}
-                        className="
-                            bg-zinc-800 
-                            rounded-lg 
-                            py-5 
-                            w-24 
-                            flex-1 
-                            flex 
-                            flex-col 
-                            items-center 
-                            gap-2 
-                            border-2 
-                            border-transparent 
-                            hover:border-brand-500 
-                            focus:border-brand-500 
-                            focus:outline-none
-                        "
-                        type="button"
-                        onClick={() => {}}
-                      >
-                        <img src={value.image.source} alt={value.image.alt} />
-                        <span>{value.title}</span>
-                      </button>
-                  );
-              })}
-          </div>
+          {!feedbackType ? (
+            <div className="flex py-8 gap-2 w-full">
+                {Object.entries(feedbackTypes).map(([key, value]) => {
+                    return (
+                        <button
+                            key={key}
+                            className="
+                                bg-zinc-800 
+                                rounded-lg 
+                                py-5 
+                                w-24 
+                                flex-1 
+                                flex 
+                                flex-col 
+                                items-center 
+                                gap-2 
+                                border-2 
+                                border-transparent 
+                                hover:border-brand-500 
+                                focus:border-brand-500 
+                                focus:outline-none
+                            "
+                            type="button"
+                            onClick={() => setFeedbackType(key as FeedbackTYpe)}
+                        >
+                            <img src={value.image.source} alt={value.image.alt} />
+                            <span>{value.title}</span>
+                        </button>
+                    );
+                })}
+            </div>
+          ) : (
+              <h1>Selecionou alguma coisa</h1>
+          )}
 
           <footer className="text-xs text-neutral-400">
               Feito com 🫀 pela <a className="underline underline-offset-2" href="https://rocektseat.com.br">Rocketseat</a>
